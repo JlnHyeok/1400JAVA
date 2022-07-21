276a37e5437bc54fb7bf9abc39ff9a4d6d4a2455
IT KOREA <gjwlsgur123@gmail.com>
2022-07-21 13:58:32 +0900

    fghj

--- src/Ch09ClassBasic/C15Prac.java
+++ src/Ch09ClassBasic/C15Prac.java
@@ -36,7 +36,7 @@ public class C15Prac {
 	   
 	   r.show();
 	   System.out.println("s의 면적은 "+s.square());
-	   if(t.contains(r)) System.out.println("t는 r을 포함합니다.");
+	   if(t.contains(r)) System.out.println("t는 r을 포함합니다..");
 	   if(t.contains(s)) System.out.println("t는 s를 포함합니다.");
 		
 		
